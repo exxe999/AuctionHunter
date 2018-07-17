d65617b4951b9005e779a00dd159308244c366e7
BuildTools <unconfigured@null.spigotmc.org>
2018-07-17 22:41:39 +0200

    first commit

---
+++ AuctionHunter/src/de/exxe/AuctionHunter/Main/Main.java
@@ -0,0 +1,17 @@
+package de.exxe.AuctionHunter.Main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+}
